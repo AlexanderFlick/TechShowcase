ea67b8909067e81e1bec94dfd47bf122e10d110c
AlexanderFlick <macaronisupermoon@gmail.com>
2022-03-31 17:58:36 -0500

    created red test for album service

---
+++ TechShowcase/TechShowcase.Tests/ServiceTests/AlbumServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechShowcase.Data;
+using TechShowcase.Data.Models;
+using TechShowcase.Services;
+using Xunit;
+
+namespace TechShowcase.Tests.ServiceTests;
+public class AlbumServiceTests
+{
+    private readonly IAlbumService _sut;
+    private readonly Mock<IAlbumRepo> _albumRepoMock;
+    private readonly Fixture _fixture = (Fixture)new Fixture().Customize(new AutoMoqCustomization());
+
+    public AlbumServiceTests()
+    {
+        _albumRepoMock = _fixture.Freeze<Mock<IAlbumRepo>>();
+        _sut = _fixture.Create<AlbumService>();
+    }
+
+    [Fact]
+    public void GivenAlbum_WhenIdExists_ThenReturnAlbum()
+    {
+        var randomInt = _fixture.Create<int>();
+        var photos = _fixture.CreateMany<Photo>().ToList();
+        var validAlbum = new Album
+        {
+            Id = randomInt,
+            Photos = photos
+        };
+        _albumRepoMock.Setup(c => c.ById(randomInt)).Returns(validAlbum);
+
+        var actual = _sut.ById(randomInt);
+
+        actual.Should().BeEquivalentTo(validAlbum);
+    }
+}
